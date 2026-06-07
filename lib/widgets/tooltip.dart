71d8dcc78790cf189832e969cdb903c71e1b8d8d
kkkunny <1401289859@qq.com>
2026-06-07 20:24:10 +0800

    feat: 抢占式tooltip

--- lib/widgets/tooltip.dart
+++ lib/widgets/tooltip.dart
@@ -5,28 +5,30 @@ void pushTooltipInfo(
   String text, {
   Duration duration = const Duration(milliseconds: 500),
 }) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      backgroundColor: Colors.black45,
-      content: Row(
-        children: [
-          Icon(Icons.info_outline_rounded, color: Colors.white),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                '提示：$text',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.black45,
+        content: Row(
+          children: [
+            Icon(Icons.info_outline_rounded, color: Colors.white),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  '提示：$text',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        duration: duration,
       ),
-      duration: duration,
-    ),
-  );
+    );
 }
 
 void pushTooltipWarning(
@@ -34,28 +36,30 @@ void pushTooltipWarning(
   String text, {
   Duration duration = const Duration(milliseconds: 500),
 }) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      backgroundColor: Colors.yellow.withValues(alpha: 0.8),
-      content: Row(
-        children: [
-          Icon(Icons.warning_amber_rounded, color: Colors.black),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                '警告：$text',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.yellow.withValues(alpha: 0.8),
+        content: Row(
+          children: [
+            Icon(Icons.warning_amber_rounded, color: Colors.black),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  '警告：$text',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        duration: duration,
       ),
-      duration: duration,
-    ),
-  );
+    );
 }
 
 void pushTooltipError(
@@ -63,28 +67,30 @@ void pushTooltipError(
   String text, {
   Duration duration = const Duration(seconds: 1),
 }) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      backgroundColor: Colors.red.withValues(alpha: 0.6),
-      content: Row(
-        children: [
-          Icon(Icons.error_outline_rounded, color: Colors.white),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                '错误：$text',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.red.withValues(alpha: 0.6),
+        content: Row(
+          children: [
+            Icon(Icons.error_outline_rounded, color: Colors.white),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  '错误：$text',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        // duration: duration,
       ),
-      // duration: duration,
-    ),
-  );
+    );
 }
 
 Future<T> tooltipNetFetch<T>(
